26084fcb4f8e91aae3c09355c37aeca635f59725
Jeff Houle <jhoule86@gmail.com>
2020-06-03 16:32:48 -0400

    add new test for ConfigHelper

---
+++ tests/php/Helpers/ConfigHelperTest.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @copyright 2020 Wayfair LLC - All rights reserved
+ */
+
+namespace Wayfair\Tests\Mappers;
+
+use Wayfair\Core\Helpers\AbstractConfigHelper;
+use Wayfair\Helpers\ConfigHelper;
+
+final class ConfigHelperTest extends \PHPUnit\Framework\TestCase {
+    
+    /**
+     * Various tests for validateItemMappingMethod
+     * 
+     * @dataProvider dataProviderForTestValidateItemMappingMethod
+     */
+    public function testValidateItemMappingMethod($input, $expected, $msg = null)
+    {
+        $result = ConfigHelper::validateItemMappingMethod($input);
+
+        $this->assertEquals($expected, $result, $msg);
+    }
+
+    public function dataProviderForTestValidateItemMappingMethod()
+    {
+        $cases = [];
+
+        $cases[] = [null, AbstractConfigHelper::ITEM_MAPPING_VARIATION_NUMBER, "null input should default to variation number"];
+
+        $cases[] = ['', AbstractConfigHelper::ITEM_MAPPING_VARIATION_NUMBER, "blank input should default to variation number"];
+
+        $cases[] = ['FakeMode', AbstractConfigHelper::ITEM_MAPPING_VARIATION_NUMBER, "bad input should default to variation number"];
+
+        foreach([AbstractConfigHelper::ITEM_MAPPING_VARIATION_NUMBER, AbstractConfigHelper::ITEM_MAPPING_SKU, AbstractConfigHelper::ITEM_MAPPING_EAN] as $mode)
+        {
+            $cases[] = [$mode, $mode, "valid input should be returned as output"];
+        }
+
+        return $cases;
+    }
+}
