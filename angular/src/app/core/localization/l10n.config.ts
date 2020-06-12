d629fc383a24b5cfdc29bcc462dcc44b04082e8b
Jeff Houle <jhoule86@gmail.com>
2020-06-11 20:18:04 -0400

    fix syntax errors

--- angular/src/app/core/localization/l10n.config.ts
+++ angular/src/app/core/localization/l10n.config.ts
@@ -1,87 +1,81 @@
-import {
-    L10nConfig,
-    ProviderType,
-    StorageStrategy
-} from 'angular-l10n';
+import { L10nConfig, ProviderType, StorageStrategy } from "angular-l10n";
 
-export const l10nConfig:L10nConfig = getL10nConfig();
+export const l10nConfig: L10nConfig = getL10nConfig();
 
-export const LANG_DE:string = 'de';
-export const LANG_EN:string = 'en';
+function getL10nConfig(): L10nConfig {
+  let langDE: string = "de";
+  let langEN: string = "en";
 
-// Plentymarkets defaults to German
-export const DEFAULT_LANG:string = LANG_DE;
-export const KNOWN_LANGS:string[] = [LANG_DE, LANG_EN];
+  // Plentymarkets defaults to German
+  let defaultLang: string = langDE;
+  let knownLangs: string[] = [langDE, langEN];
 
-function getL10nConfig():L10nConfig
-{
-    let langInLocalStorage:string = localStorage.getItem('plentymarkets_lang_');
-    let lang:string = langInLocalStorage;
+  let langInLocalStorage: string = localStorage.getItem("plentymarkets_lang_");
+  let resolvedLang: string = langInLocalStorage;
 
-    if (lang == null)
-    {
-        // ask browser for perferred language
-        lang = navigator.language.slice(0, 2).toLocaleLowerCase();
+  if (resolvedLang == null) {
+    // ask browser for perferred language
+    resolvedLang = navigator.language.slice(0, 2).toLocaleLowerCase();
 
-        // as of 1.1.2, no longer (re)defining 'plentymarkets_lang_'
-        // because we do not own the plentymarkets-wide language
-    }
+    // as of 1.1.2, no longer (re)defining 'plentymarkets_lang_'
+    // because we do not own the plentymarkets-wide language
+  }
 
-    if(!(KNOWN_LANGS.includes(lang)))
-    {
-        lang = DEFAULT_LANG;
-    }
+  if (!knownLangs.includes(resolvedLang)) {
+    resolvedLang = defaultLang;
+  }
 
-    // default to production settings
-    let prefix:string = 'assets/lang/locale-';
-    let terraComponentsLocalePrefix:string = 'assets/lang/terra-components/locale-';
+  // default to production settings
+  let prefix: string = "assets/lang/locale-";
+  let terraComponentsLocalePrefix: string =
+    "assets/lang/terra-components/locale-";
 
-    if(process.env.ENV !== 'production')
-    {
-        // settings for local deployments
-        prefix = 'src/app/assets/lang/locale-';
-        terraComponentsLocalePrefix = 'node_modules/@plentymarkets/terra-components/app/assets/lang/locale-';
-    }
+  if (process.env.ENV !== "production") {
+    // settings for local deployments
+    prefix = "src/app/assets/lang/locale-";
+    terraComponentsLocalePrefix =
+      "node_modules/@plentymarkets/terra-components/app/assets/lang/locale-";
+  }
 
-    // build the config json.
-    //
-    // - using StorageStrategy.Local
-    //      * we are matching the Plentymarkets language in local storage
-    //      * we should NOT be defining the language for this site at a larger scope,
-    //      * our language settings should not persist after use
-    //
-    // - specify DE before EN
-    //      * matches Plentymarkets
+  // build the config json.
+  //
+  // - using StorageStrategy.Local
+  //      * we are matching the Plentymarkets language in local storage
+  //      * we should NOT be defining the language for this site at a larger scope,
+  //      * our language settings should not persist after use
+  //
+  // - specify DE before EN
+  //      * matches Plentymarkets
 
-    return {
-        locale:      {
-            languages: [
-                {
-                    code: LANG_DE,
-                    dir:  'ltr'
-                },
-                {
-                    code: LANG_EN,
-                    dir:  'ltr'
-                }
-            ],
-            language:  lang,
-            storage:   StorageStrategy.Local
+  return {
+    locale: {
+      languages: [
+        {
+          code: langDE,
+          dir: "ltr",
         },
-        translation: {
-            providers:            [
-                {
-                    type:   ProviderType.Static,
-                    prefix: prefix
-                },
-                {
-                    type:   ProviderType.Static,
-                    prefix: terraComponentsLocalePrefix
-                }
-            ],
-            caching:              true,
-            composedKeySeparator: '.',
-            i18nPlural:           false
-        }
-    };
+        {
+          code: langEN,
+          dir: "ltr",
+        },
+      ],
+      language: resolvedLang,
+      storage: StorageStrategy.Local,
+    },
+    translation: {
+      providers: [
+        {
+          type: ProviderType.Static,
+          prefix: prefix,
+        },
+        {
+          type: ProviderType.Static,
+          prefix: terraComponentsLocalePrefix,
+        },
+      ],
+      caching: true,
+      composedKeySeparator: ".",
+      i18nPlural: false,
+    },
+  };
 }
