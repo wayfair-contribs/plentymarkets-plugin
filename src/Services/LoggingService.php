00213fcd79eb4c2dab9b1674b6b849fc05f0bdf5
Jeff Houle <jhoule86@gmail.com>
2020-06-24 13:14:55 -0400

    remove logjam

--- src/Services/LoggingService.php
+++ src/Services/LoggingService.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * @copyright 2020 Wayfair LLC - All rights reserved
  */
 
 namespace Wayfair\Services;
 
-use Exception;
 use Plenty\Plugin\Log\Loggable;
 
 use Wayfair\Core\Api\Services\LogSenderService;
@@ -13,7 +13,8 @@ use Wayfair\Core\Contracts\LoggerContract;
 use Wayfair\Core\Helpers\AbstractConfigHelper;
 use Wayfair\Models\ExternalLogs;
 
-class LoggingService implements LoggerContract {
+class LoggingService implements LoggerContract
+{
   use Loggable;
 
   const DEBUG = 'DEBUG';
@@ -40,7 +41,9 @@ class LoggingService implements LoggerContract {
   /**
    * Initialize a logging service object
    */
-  public function __construct() {
+  public function __construct()
+  {
+    /** @var AbstractConfigHelper */
     $this->configHelper = pluginApp(AbstractConfigHelper::class);
     $this->version = $this->configHelper->getPluginVersion();
   }
@@ -51,12 +54,8 @@ class LoggingService implements LoggerContract {
    * @param string $code
    * @param null   $loggingInfo
    */
-  public function debug(string $code, $loggingInfo = null) {
-
-    if (! $this->canLogLowerThanError())
-    {
-      return;
-    }
+  public function debug(string $code, $loggingInfo = null)
+  {
 
     list($additionalInfo, $method, $referenceType, $referenceValue) = $this->extractVars($loggingInfo);
     $this->getPlentyMarketLoggerInstance($method, $referenceType, $referenceValue)->debug($code, $additionalInfo);
@@ -68,12 +67,8 @@ class LoggingService implements LoggerContract {
    * @param string $code
    * @param null   $loggingInfo
    */
-  public function info(string $code, $loggingInfo = null) {
-
-    if (! $this->canLogLowerThanError())
-    {
-      return;
-    }
+  public function info(string $code, $loggingInfo = null)
+  {
 
     list($additionalInfo, $method, $referenceType, $referenceValue) = $this->extractVars($loggingInfo);
     $this->getPlentyMarketLoggerInstance($method, $referenceType, $referenceValue)->info($code, $additionalInfo);
@@ -85,7 +80,8 @@ class LoggingService implements LoggerContract {
    * @param string $code
    * @param null   $loggingInfo
    */
-  public function error(string $code, $loggingInfo = null) {
+  public function error(string $code, $loggingInfo = null)
+  {
     list($additionalInfo, $method, $referenceType, $referenceValue) = $this->extractVars($loggingInfo);
     $this->getPlentyMarketLoggerInstance($method, $referenceType, $referenceValue)->error($code, $additionalInfo);
   }
@@ -96,12 +92,8 @@ class LoggingService implements LoggerContract {
    * @param string $code
    * @param null   $loggingInfo
    */
-  public function warning(string $code, $loggingInfo = null) {
-
-    if (! $this->canLogLowerThanError())
-    {
-      return;
-    }
+  public function warning(string $code, $loggingInfo = null)
+  {
 
     list($additionalInfo, $method, $referenceType, $referenceValue) = $this->extractVars($loggingInfo);
     $this->getPlentyMarketLoggerInstance($method, $referenceType, $referenceValue)->warning($code, $additionalInfo);
@@ -114,7 +106,8 @@ class LoggingService implements LoggerContract {
    *
    * @return \Plenty\Log\Contracts\LoggerContract
    */
-  private function getPlentyMarketLoggerInstance(string $method, string $referenceType = null, int $referenceValue = null) {
+  private function getPlentyMarketLoggerInstance(string $method, string $referenceType = null, int $referenceValue = null)
+  {
     $pmLoggerInstance = $this->getLogger($method);
     if (isset($referenceValue)) {
       $pmLoggerInstance = $pmLoggerInstance->setReferenceValue($referenceValue);
@@ -133,7 +126,9 @@ class LoggingService implements LoggerContract {
    *
    * @return array
    */
-  public function extractVars($loggingInfo): array {
+  public function extractVars($loggingInfo): array
+  {
+    /** @var ExternalLogs */
     $externalLogs = pluginApp(ExternalLogs::class);
     $clientID = $this->configHelper->getClientId();
     $shortMessage = [];
@@ -144,17 +139,16 @@ class LoggingService implements LoggerContract {
       $referenceValue = (int) $loggingInfo['referenceValue'] ?? null;
 
       if (strlen(json_encode($loggingInfo)) > self::STRING_LIMIT) {
-          $logForKibana = [];
-          $shortMessage['message'] = self::LOG_KEY_MESSAGE_TO_LONG . $clientID . '-' . date('M d Y H:i:s');
-          $additionalInfo = $shortMessage;
-          $logForKibana['message'] = self::LOG_KEY_MESSAGE_TO_LONG . $clientID . '-' . date('D, d M Y H:i:s');
-          $logForKibana['details'] = $loggingInfo;
-          $externalLogs->addErrorLog(json_encode($logForKibana));
+        $logForKibana = [];
+        $shortMessage['message'] = self::LOG_KEY_MESSAGE_TO_LONG . $clientID . '-' . date('M d Y H:i:s');
+        $additionalInfo = $shortMessage;
+        $logForKibana['message'] = self::LOG_KEY_MESSAGE_TO_LONG . $clientID . '-' . date('D, d M Y H:i:s');
+        $logForKibana['details'] = $loggingInfo;
+        $externalLogs->addErrorLog(json_encode($logForKibana));
       }
       $additionalInfo[self::WAYFAIR_PLUGIN_VERSION] = $this->version;
 
       return array($additionalInfo, $method, $referenceType, $referenceValue);
-
     } finally {
       if (count($externalLogs->getLogs())) {
         /** @var LogSenderService $logSenderService */
@@ -163,22 +157,4 @@ class LoggingService implements LoggerContract {
       }
     }
   }
-
-  /**
-   * Checks if it is alright to log at a level lower than ERROR.
-   * Logging at levels such as INFO and DEBUG prior to the end of the "boot" period
-   * Causes issues.
-   *
-   * See https://forum.plentymarkets.com/t/wayfair-log-levels-info-and-debug-not-working-for-loggable-module/581320/22
-   *
-   * @return boolean
-   */
-  private function canLogLowerThanError(): bool {
-    /**
-     * @var AbstractConfigHelper $configHelper
-     */
-    $configHelper = pluginApp(AbstractConfigHelper::class);
-    return $configHelper->hasBooted();
-  }
-
 }
