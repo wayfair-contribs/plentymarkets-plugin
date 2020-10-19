e22369446c15c421f62feac975f88dcbed42eebe
Jeff Houle <jhoule86@gmail.com>
2020-10-19 15:27:54 -0400

    remove state on warehouses page when a button is clicked

--- angular/src/app/views/warehouseSupplier/warehouseSupplier.component.ts
+++ angular/src/app/views/warehouseSupplier/warehouseSupplier.component.ts
@@ -1,95 +1,126 @@
-import {
-    Component,
-    Input,
-    OnInit
-} from '@angular/core';
-import { Language, TranslationService } from 'angular-l10n';
-import { WarehouseSupplierService } from '../../core/services/warehouseSupplier/warehouseSupplier.service';
-import { WarehouseService } from '../../core/services/warehouse/warehouse.service';
+import { Component, OnInit } from "@angular/core";
+import { Language, TranslationService } from "angular-l10n";
+import { WarehouseSupplierService } from "../../core/services/warehouseSupplier/warehouseSupplier.service";
+import { WarehouseService } from "../../core/services/warehouse/warehouse.service";
 
 @Component({
-    selector: 'warehouse-supplier',
-    template: require('./warehouseSupplier.component.html'),
-    styles: [require('./warehouseSupplier.component.scss')],
+  selector: "warehouse-supplier",
+  template: require("./warehouseSupplier.component.html"),
+  styles: [require("./warehouseSupplier.component.scss")],
 })
 export class WarehouseSupplierComponent implements OnInit {
-    @Language()
-    public lang: string;
+  @Language()
+  public lang: string;
 
-    public warehouseSuppliers = [];
+  public warehouseSuppliers = [];
 
-    public removedWarehouseSuppliers = [];
+  public removedWarehouseSuppliers = [];
 
-    public warehouses = [];
+  public warehouses = [];
 
-    public status = { type: null, value: null };
+  public status = { type: null, value: null };
 
-    constructor(private warehouseService: WarehouseService, private warehouseSupplierService: WarehouseSupplierService, private translation:TranslationService) {
-    }
+  constructor(
+    private warehouseService: WarehouseService,
+    private warehouseSupplierService: WarehouseSupplierService,
+    private translation: TranslationService
+  ) {}
 
-    public ngOnInit(): void {
-        this.warehouseService.fetch()
-            .subscribe(data => {
-                this.warehouses = data
-            }, err => {
-                this.status.type = 'text-danger'
-                this.status.value = this.translation.translate('error_fetch')
-            })
+  public ngOnInit(): void {
+    this.loadFromBackend();
+  }
 
-        this.warehouseSupplierService.fetchMappings()
-            .subscribe(data => {
-                this.warehouseSuppliers = data
-            }, err => {
-                this.status.type = 'text-danger'
-                this.status.value = this.translation.translate('error_fetch')
-            })
-    }
+  public changeToFetchErrorStatus(): void {
+    this.status.type = "text-danger";
+    this.status.value = this.translation.translate("error_fetch");
+  }
 
-    public saveMappings(): void {
-        let self = this
-        let validated = true
-        this.warehouseSuppliers.forEach((item) => {
-            // Check if any value is empty.
-            if (Object.values(item).some(itemValue => !itemValue)) {
-                self.status.type = 'text-danger'
-                self.status.value = this.translation.translate('empty_values_mapping')
-                validated = false
-            }
-        })
-        if(!validated) return;
-        if (this.warehouseSuppliers.length > 0 || this.removedWarehouseSuppliers.length > 0) {
-            this.status.type = 'text-info'
-            this.status.value = this.translation.translate('saving_status')
-            let data = this.removedWarehouseSuppliers.concat(this.warehouseSuppliers);
-            this.warehouseSupplierService.postMappings(data).subscribe(data => {
-                this.status.type = 'text-info'
-                this.status.value = this.translation.translate('saved')
-            }, err => {
-                this.status.type = 'text-danger'
-                this.status.value = this.translation.translate('error_save')
-            });
-        } else {
-            this.status.type = 'text-info'
-            this.status.value = this.translation.translate('nothing_to_save')
+  public clearStatus(): void {
+    this.status.type = "";
+    this.status.value = "";
+  }
+
+  public loadFromBackend(): void {
+    this.clearStatus();
+    this.warehouseService.fetch().subscribe(
+      (data) => {
+        this.warehouses = data;
+      },
+      (err) => {
+        this.status.type = "text-danger";
+        this.status.value = this.translation.translate("error_fetch");
+        this.changeToFetchErrorStatus();
+      }
+    );
+
+    this.warehouseSupplierService.fetchMappings().subscribe(
+      (data) => {
+        this.warehouseSuppliers = data;
+      },
+      (err) => {
+        this.changeToFetchErrorStatus();
+      }
+    );
+  }
+
+  public saveMappings(): void {
+    this.clearStatus();
+    this.warehouseSuppliers.forEach((item) => {
+      // Check if any value is empty.
+      if (Object.values(item).some((itemValue) => !itemValue)) {
+        this.status.type = "text-danger";
+        this.status.value = this.translation.translate("empty_values_mapping");
+        return;
+      }
+    });
+
+    if (
+      this.warehouseSuppliers.length > 0 ||
+      this.removedWarehouseSuppliers.length > 0
+    ) {
+      this.status.type = "text-info";
+      this.status.value = this.translation.translate("saving_status");
+      let data = this.removedWarehouseSuppliers.concat(this.warehouseSuppliers);
+      this.warehouseSupplierService.postMappings(data).subscribe(
+        (data) => {
+          this.status.type = "text-info";
+          this.status.value = this.translation.translate("saved");
+        },
+        (err) => {
+          this.status.type = "text-danger";
+          this.status.value = this.translation.translate("error_save");
         }
+      );
+    } else {
+      this.status.type = "text-info";
+      this.status.value = this.translation.translate("nothing_to_save");
     }
+  }
 
-    public addMapping(): void {
-        this.warehouseSuppliers.push({
-            supplierId: null,
-            warehouseId: null
-        })
-    }
+  public addMapping(): void {
+    this.clearStatus();
+    this.warehouseSuppliers.push({
+      supplierId: null,
+      warehouseId: null,
+    });
+  }
 
-    public removeMapping(warehouseSupplier): void {
-        let foundIndex = this.warehouseSuppliers.findIndex((elem, idx) => {
-            return elem.supplierId == warehouseSupplier.supplierId && elem.warehouseId == warehouseSupplier.warehouseId;
-        })
-        let targetWarehouseSupplier = this.warehouseSuppliers[foundIndex]
-        if(targetWarehouseSupplier.supplierId && targetWarehouseSupplier.warehouseId){
-            targetWarehouseSupplier.removed = true
-            this.removedWarehouseSuppliers.push(targetWarehouseSupplier)
-        }
-        this.warehouseSuppliers.splice(foundIndex, 1);
+  public removeMapping(warehouseSupplier): void {
+    this.clearStatus();
+    let foundIndex = this.warehouseSuppliers.findIndex((elem, idx) => {
+      return (
+        elem.supplierId == warehouseSupplier.supplierId &&
+        elem.warehouseId == warehouseSupplier.warehouseId
+      );
+    });
+    let targetWarehouseSupplier = this.warehouseSuppliers[foundIndex];
+    if (
+      targetWarehouseSupplier.supplierId &&
+      targetWarehouseSupplier.warehouseId
+    ) {
+      targetWarehouseSupplier.removed = true;
+      this.removedWarehouseSuppliers.push(targetWarehouseSupplier);
     }
+    this.warehouseSuppliers.splice(foundIndex, 1);
+  }
 }
