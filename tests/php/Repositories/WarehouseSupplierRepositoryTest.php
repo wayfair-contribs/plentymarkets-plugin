6794df92649d1a7543587e7a42a14a0e91c09d21
Jeff Houle <jhoule86@gmail.com>
2020-11-02 20:53:22 -0500

    rollup changes from prior branch

---
+++ tests/php/Repositories/WarehouseSupplierRepositoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @copyright 2020 Wayfair LLC - All rights reserved
+ */
+
+namespace Wayfair\Repositories;
+
+$plentymocketsFactoriesDirPath = dirname(__DIR__) . DIRECTORY_SEPARATOR
+    . 'lib' . DIRECTORY_SEPARATOR
+    . 'plentymockets' . DIRECTORY_SEPARATOR
+    . 'Factories' . DIRECTORY_SEPARATOR;
+
+class WarehouseSupplierRepositoryTest
+{
+    public function testWarehouseExists()
+    {
+        // TODO: implement
+    }
+
+    public function testFindWarehouseIds()
+    {
+        // TODO: implement
+    }
+}
---
+++ tests/php/Services/CreateOrderServiceTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * @copyright 2020 Wayfair LLC - All rights reserved
+ */
+
+namespace Wayfair\Services;
+
+$plentymocketsFactoriesDirPath = dirname(__DIR__) . DIRECTORY_SEPARATOR
+    . 'lib' . DIRECTORY_SEPARATOR
+    . 'plentymockets' . DIRECTORY_SEPARATOR
+    . 'Factories' . DIRECTORY_SEPARATOR;
+
+class CreateOrderServiceTest
+{
+    public function testCreate()
+    {
+        // TODO: implement
+    }
+}
