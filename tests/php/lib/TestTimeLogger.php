136465a257de7c58ded653e2671c2a6524538c86
Jeff Houle <jhoule86@gmail.com>
2020-12-15 15:15:43 -0500

    hide some logs by default during test

--- tests/php/lib/TestTimeLogger.php
+++ tests/php/lib/TestTimeLogger.php
@@ -10,24 +10,44 @@ use Wayfair\Core\Contracts\LoggerContract;
 
 class TestTimeLogger implements LoggerContract
 {
+    private $enableDebug;
+    private $enableInfo;
+    private $enableWarning;
+    private $enableError;
+
+    public function __construct($enableError = true, $enableWarning = true, $enableInfo = false, $enableDebug = false)
+    {
+        $this->enableError = $enableError;
+        $this->enableWarning = $enableWarning;
+        $this->enableInfo = $enableInfo;
+        $this->enableDebug = $enableDebug;
+    }
 
     public function debug(string $code, $loggingInfo = null)
     {
-        printf("\n[DEBUG]: %s\n", $code);
+        if ($this->enableDebug) {
+            printf("\n[DEBUG]: %s\n", $code);
+        }
     }
 
     public function info(string $code, $loggingInfo = null)
     {
-        printf("\n[INFO]: %s\n", $code);
+        if ($this->enableInfo) {
+            printf("\n[INFO]: %s\n", $code);
+        }
     }
 
     public function error(string $code, $loggingInfo = null)
     {
-        printf("\n[ERROR]: %s\n", $code);
+        if ($this->enableError) {
+            printf("\n[ERROR]: %s\n", $code);
+        }
     }
 
     public function warning(string $code, $loggingInfo = null)
     {
-        printf("\n[WARNING]: %s\n", $code);
+        if ($this->enableWarning) {
+            printf("\n[WARNING]: %s\n", $code);
+        }
     }
 }
